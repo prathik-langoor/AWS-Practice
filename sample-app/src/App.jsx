24017a103124cd800398cae3e874c3b9774ce75f
prathik-unilever <prathik.r@unilever.com>
2026-06-25 17:16:08 +0530

    added Image Asset

--- sample-app/src/App.jsx
+++ sample-app/src/App.jsx
@@ -1,13 +1,18 @@
 
 import './App.css'
+import logo from './assets/Logo.png'
 
 function App() {
-
   return (
     <>
-      <section>
-        <h1>Smaple App for hosting in AWS</h1>
-        </section>
+      <section style={{ backgroundColor: 'white' }}>
+        <h1 style={{ color: 'black' }}>Sample App for hosting in AWS</h1>
+        <img
+          style={{ display: 'block', width: '400px', height: '400px', margin: '20px auto' }}
+          alt="Sample Image"
+          src={logo}
+        />
+      </section>
     </>
   )
 }
