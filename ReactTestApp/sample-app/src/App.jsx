8b0c20fe483f5f1cef59c4dd1b396e8abce4da84
prathik-unilever <prathik.r@unilever.com>
2026-06-25 16:58:46 +0530

    Initial Commit

---
+++ ReactTestApp/sample-app/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section>
+        <h1>Smaple App for hosting in AWS</h1>
+        </section>
+    </>
+  )
+}
+
+export default App
